4b471655972d95c23bae82d58f2825d9830a21d4
Mojahid Hassan <152623230+Mojahid65@users.noreply.github.com>
2025-02-02 14:41:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.getElementById('imageInput').addEventListener('change', function () {
 
     fetch('https://api.remove.bg/v1.0/removebg', {
         method: 'POST',
-        headers: { 'X-Api-Key': 'YOUR_API_KEY' }, // Replace with your API Key
+        headers: { 'X-Api-Key': 'kt1KYxZ2XEtdjppkiGtZ9qMg' }, // Replace with your API Key
         body: formData
     })
     .then(response => {
